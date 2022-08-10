8831f5ac089d0647a14631775a7b8c04bf26d022
Laydo <robinsonlanden@gmail.com>
2022-08-10 19:57:47 +0100

    Digital Ocean Updates
    - Updated various settings.py properties to configure for DO deployment
    - Not sure on database updates but it still works locally

--- homeapps/settings.py
+++ homeapps/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
+import os
+import sys
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xpmfrzo9qo((i4v3)qa@f_kzitu#w2h^gf$k26zgq!2op$049u'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key()) #'django-insecure-xpmfrzo9qo((i4v3)qa@f_kzitu#w2h^gf$k26zgq!2op$049u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['10.0.0.21','localhost']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS','10.0.0.21,localhost').split(',')
+
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True" # Comment out and set to True for Pi4
 
 
 # Application definition
@@ -84,16 +89,23 @@ WSGI_APPLICATION = 'homeapps.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'homeapps',
-        'USER': 'laydo',
-        'PASSWORD': 'drew758Met340^',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'homeapps',
+            'USER': 'laydo',
+            'PASSWORD': 'drew758Met340^',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
 }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABSE_URL environment variable not defined")
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 
 # Password validation
